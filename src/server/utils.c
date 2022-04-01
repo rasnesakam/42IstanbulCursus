23b18ccbe665473abd706c226052bd77f5a4ea6f
ensar <ensar.makas@gmail.com>
2022-04-01 14:35:46 +0300

    asdasd

--- src/server/utils.c
+++ src/server/utils.c
@@ -6,11 +6,26 @@ void handle(int signal){
     static int  bsize;
 
     if (signal == SIGUSR2)
-        clock = 1;
+    {
+        if (clock == NULL || clock == 0)
+            clock = 1;
+        else {
+            data = (data << 1) | 0;
+            bsize++;
+        }
+    }
     if (signal == SIGUSR1)
     {
-        
+        if (data == NULL)
+            data = 0;
+        data = (data << 1) | 1;
+        bsize++;
+        clock = 0;
     }
 
+    if (bsize == 8){
+        ft_putchar_fd(data,1);
+        bsize = 0;
+    }
     
 }
