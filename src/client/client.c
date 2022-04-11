bb7b7a54dc2bfbfac098fd72450aaeceffac07d4
ensar <ensar.makas@gmail.com>
2022-04-11 13:21:55 +0300

    Deficients clearified

--- src/client/client.c
+++ src/client/client.c
@@ -6,7 +6,7 @@
 /*   By: emakas <rasnesakam@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 10:12:58 by emakas            #+#    #+#             */
-/*   Updated: 2022/04/11 13:01:31 by emakas           ###   ########.fr       */
+/*   Updated: 2022/04/11 13:16:08 by emakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int	main(int argc, char **argv)
 		signal (SIGUSR1, handle);
 		ft_sendmsg (pid, message);
 		ft_sendmsg (pid, ft_itoa(getpid()));
-		ft_putnbr_fd(getpid(),1);
 		while (1)
 			pause();
 	}
