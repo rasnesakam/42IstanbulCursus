e1dbafebcb7823464264b8a8e2edb585e42f3127
Ensar Makas <ensar.makas@gmail.com>
2022-06-13 20:13:15 +0300

    don't know what im doing

--- src/mapresolver/mapresolver.h
+++ src/mapresolver/mapresolver.h
@@ -6,7 +6,7 @@
 /*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/01 15:44:22 by emakas            #+#    #+#             */
-/*   Updated: 2022/05/17 13:58:49 by emakas           ###   ########.fr       */
+/*   Updated: 2022/06/11 22:26:39 by emakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,42 +19,13 @@
 # include <fcntl.h>
 
 /**
- * resolve_map(mlx_vars,char *);
+ * @brief Create a map model object
  * 
- * @param main function for rendering string maps into window
- * @param mlx: structure that stores mlx library variables
- * @param maddr: map file address
+ * @param file 
+ * @return t_object*** An array of the objects with 3 dimension (x,y,z=2)
  */
-void	resolve_map(t_mlx mlx, char *maddr);
+t_object ***create_map_model(char *file);
 
-/**
- * validate_map(char*)
- * 
- * @brief Vadidates the map file with given path
- *
- * @param maddr: Map file address
- * @param xsize: Horizontal size for map. Will be set after validation
- * @param ysize: Vertical size for map. Will be set after validation
- */
-int		validate_map(char *maddr, int *xsize, int *ysize);
-
-/**
- * @brief Render line for mapping
- *
- * @param mlx Minilibx var for interacting with window
- * @param lindex Line index
- * @param line The line will be rendering
- */
-void	renderline(t_mlx mlx, int lindex, char *line);
-
-/**
- * @brief Render chars in line for map
- * 
- * @param mlx Minilibx var for interacting with window
- * @param lindex Line index
- * @param cindex Column index
- * @param code Char code for objects in map
- */
-void	renderpoint(t_mlx mlx, int lindex, int cindex, char code);
+t_object ***move_object(t_object ***map, int x, int y);
 
 #endif
--- src/structures.h
+++ src/structures.h
@@ -1,11 +1,13 @@
 
 #ifndef STRUCTURES_H
 # define STRUCTURES_H
+# include "objects/objects.h"
 
 typedef struct s_mlx
 {
 	void	*mlx;
 	void	*win;
+	t_object **mmodel[2];
 }
 t_mlx;
 
