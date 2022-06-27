d714a91efb2c1fe37bbe29a02abf504be5e487c6
Ensar Makas <ensar.makas@gmail.com>
2022-06-27 10:25:02 +0300

    stack data structure building

---
+++ src/stack/stack.h
@@ -0,0 +1,16 @@
+
+#ifndef STACK_H
+# define STACK_H
+
+typedef struct s_stack{
+	int	*stack;
+	int	top;
+	int	size;
+} t_stack;
+
+t_stack	create_stack(int size);
+
+void	push_stack(t_stack *stack, int val);
+int		pop_stack(t_stack *stack);
+
+#endif
