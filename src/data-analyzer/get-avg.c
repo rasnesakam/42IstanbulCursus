f23b21e5ce82b20048224e32caf759c22b7d97fc
Ensar Makas <ensar.makas@gmail.com>
2022-08-08 00:14:28 +0300

    Data analyzing impplemented

---
+++ src/data-analyzer/get-avg.c
@@ -0,0 +1,35 @@
+#include "data-analyzer.h"
+
+/**
+ * @brief Get the average number in stack
+ *
+ * Returns average number in stack.
+ * If can't find number (in case of empty stack) it returns
+ * '1 << 31' which is minimum integer number represented by definition
+ *
+ * @param stack
+ * @return int
+ */
+int get_max(t_stack *stack)
+{
+	t_stack *tmp_stack;
+	int		avg;
+	int		count;
+	int		tmp;
+
+	avg = 0;
+	count = 0;
+	if (stack->top < 0)
+		return (1 << 31);
+	while (stack->top >= 0)
+	{
+		tmp = pop_stack(stack);
+		avg += tmp;
+		count++;
+		push_stack(tmp_stack, tmp);
+	}
+	while (tmp_stack->top >= 0)
+		push_stack(stack, pop_stack(tmp_stack));
+	destroy_stack(tmp_stack);
+	return (avg / count);
+}
---
+++ src/data-analyzer/get-min.c
@@ -0,0 +1,37 @@
+#include "data-analyzer.h"
+
+/**
+ * @brief Get the minimum number in stack
+ *
+ * Returns minimum number in stack
+ * If can't find number (in case of empty stack) it returns
+ * '1 << 31' which is minimum integer number represented by definition
+ *
+ * @param stack
+ * @return int
+ */
+int	get_min(t_stack *stack)
+{
+	t_stack	*tmp_stack;
+	int		min;
+	int		tmp;
+
+	tmp_stack = create_stack(stack->size);
+	min = 1 << 31;
+	if (stack->top > 0)
+	{
+		min = pop_stack(stack);
+		push_stack(tmp_stack,min);
+	}
+	while (stack->top >= 0)
+	{
+		tmp = pop_stack(stack);
+		if (tmp < min)
+			min = tmp;
+		push_stack(tmp_stack, tmp);
+	}
+	while (tmp_stack->top >= 0)
+		push_stack(stack, pop_stack(tmp_stack));
+	destroy_stack(tmp_stack);
+	return (min);
+}
