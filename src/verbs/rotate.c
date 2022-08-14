ef01c97e428ce547a7c45644979bfc2943b286ff
Ensar Makas <ensar.makas@gmail.com>
2022-08-14 10:25:12 +0300

    Bir sana yandım ben. Bir sana vicdansız

--- src/verbs/rotate.c
+++ src/verbs/rotate.c
@@ -1,13 +1,18 @@
 #include "verbs.h"
 #include <libft.h>
+#include "../utils/utils.h"
 
 void	rotate(t_stack *stack)
 {
 	int		value;
 	t_stack	*tmp_stack;
 
+	if (stack->top <= 0)
+		return ;
 	value = pop_stack(stack);
 	tmp_stack = create_stack(stack->top + 1);
+	if (!tmp_stack)
+		ft_error("COULDN'T CREATED TEMPORARY STACK");
 	while (stack->top >= 0)
 		push_stack(tmp_stack, pop_stack(stack));
 	push_stack(stack, value);
