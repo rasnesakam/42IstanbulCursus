27a0149f453ebf252b1dbec068c82896e9ccac96
ensar <ensar.makas@gmail.com>
2022-08-22 16:31:51 +0300

    AKILSIZ BAŞIN CEZASINI AYAKLAR ÇEKER
    - Forget to fix one norminette error :c

--- bonus/checker/checker_bonus.h
+++ bonus/checker/checker_bonus.h
@@ -6,7 +6,7 @@
 /*   By: emakas <emakas@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/21 11:16:54 by emakas            #+#    #+#             */
-/*   Updated: 2022/08/22 15:48:48 by emakas           ###   ########.fr       */
+/*   Updated: 2022/08/22 16:31:40 by emakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include "../arg-converter/arg_converter_bonus.h"
 # include "../stack/stack_bonus.h"
 # include "../verbs/verbs_bonus.h"
-#include <get_next_line.h>
+# include <get_next_line.h>
 
 void	start_checker(t_stack *stack_a, t_stack *stack_b);
 
