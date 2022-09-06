f1ccc97eccf2e9c7ed16d1701efb10b868022340
Ensar Makas <ensar.makas@gmail.com>
2022-09-06 21:21:33 +0300

    corrected philo-think

--- src/utils/philo-think.c
+++ src/utils/philo-think.c
@@ -6,6 +6,6 @@ void *philo_think(t_environment *environment)
 	pthread_mutex_t *mutex;
 
 	mutex = &(environment->philosopher->mutex);
-	if (!*((int *)get_synchronized(mutex,philo_is_dead, environment->philosopher)))
+	if (get_int_sync(mutex, philo_is_dead, (void *)environment->philosopher))
 		philo_print(*(environment->philosopher), "is thinking");
 }
