176a4e41374e322601c8072dad627c9f5d91e5ca
emakas <rasnesakam@gmail.com>
2022-09-07 19:25:39 +0300

    Reimplemented philo-eat

--- src/utils/philo-eat.c
+++ src/utils/philo-eat.c
@@ -6,7 +6,7 @@
 /*   By: emakas <rasnesakam@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 16:31:16 by emakas            #+#    #+#             */
-/*   Updated: 2022/09/07 18:02:29 by emakas           ###   ########.fr       */
+/*   Updated: 2022/09/07 18:39:14 by emakas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	*prepare_eat(t_environment *env)
 	pthread_mutex_t	*mutex;
 
 	mutex = env->forks[0];
+	philo_print(*env, "is taken a fork");
 	result = get_synchronized(mutex,
 		(void *(*)(void *))start_eat, (void *)env);
 	return (result);
@@ -25,13 +26,22 @@ void	*prepare_eat(t_environment *env)
 
 void	*start_eat(t_environment *env)
 {
-	pthread_mutex_t	*mutex;
-	t_philosopher	*philo;
+	unsigned long long	timestamp;
+	pthread_mutex_t		*mutex;
+	t_philosopher		*philo;
+	t_bifunction		bifunction;
 
 	mutex = &(env->philosopher->mutex);
 	philo = env->philosopher;
 	if (get_int_sync(mutex, (int (*)(void *))philo_is_dead, (void *) philo))
 		return (NULL);
-	philo_print()
+	philo_print(*env, "is taken a fork");
+	philo_print(*env, "is eating");
+	usleep((env->eat_time) * 1000);
+	timestamp = get_timestamp(env->start_time);
+	bifunction.bifunc = (void (*)(void *, void *)) set_philo_last_eat;
+	bifunc.arg_1 = (void *) philo;
+	bifunc.arg_2 = (void *) timestamp;
+	call_synchronized(&(philo->mutex), call_bifunction, bifunction);
 	return ((void *)philo);
 }
