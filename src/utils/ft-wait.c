deb674e1cd8bc8d8628958f6fa7ffc94177986e3
Ensar Makas <ensar.makas@gmail.com>
2022-09-10 15:35:23 +0300

    trying to improve ft_wait v2

--- src/utils/ft-wait.c
+++ src/utils/ft-wait.c
@@ -9,9 +9,9 @@ void	ft_wait(unsigned int miliseconds)
 	while (count <= miliseconds)
 	{
 		subcount = 1;
-		while (subcount <= 10)
+		while (subcount <= 100)
 		{
-			usleep(100);
+			usleep(10);
 			subcount++;
 		}
 		count++;
