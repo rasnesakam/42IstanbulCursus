d6a734f2d1aa4ff243a01807688cc601d6dc9c7f
ensar <ensar.makas@gmail.com>
2023-03-20 20:43:54 +0300

    work on replace

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -4,44 +4,68 @@
 
 class Degistirinator {
     private:
-        std::string substitude;
+        std::string search;
         std::string replacement;
     public:
-        Degistirinator(std::string substitude, std::string replacement){
-            this->substitude = substitude;
+        Degistirinator(std::string search, std::string replacement){
+            this->search = search;
             this->replacement = replacement;
         };
         std::string replace(std::string input) {
-            return input;
+            std::string str = "";
+            int founded = input.find(this->replacement);
+            int cursor = 0;
+            if (founded =-1)
+                    return (input);
+            while (founded > -1){
+                founded = input.find(this->replacement);
+                if (founded == -1)
+                    founded = input.length();
+                for (; cursor < founded; cursor++)
+                    str += input[cursor];
+                if (cursor < input.length())
+                    str += this->replacement;
+                cursor += this->replacement.length();
+            }
+            return str;
         }
 };
 
 class FileWriter {
     private:
+        std::string fileName;
         std::ofstream outFile;
     public:
-        FileWriter(std::string filename) : outFile(std::ofstream(filename)) {
-            this->outFile = std::ofstream("");
+        FileWriter(std::string fileName) {
+            this->fileName = fileName;
+            this->outFile.open(fileName);
         }
+        /*
+        FileWriter(const FileWriter& writer) {
+            this->fileName = writer.fileName;
+            this->outFile.open(this->fileName);
+        }
+        */
         ~FileWriter(){
             this->outFile.close();
         }
         void writeLine(std::string input){
-            this->outFile << input;
+            this->outFile << input << std::endl;
+        }
+        std::string getFileName(){
+            return this->fileName;
         }
 };
 
-std::string withFile(std::string file1, Degistirinator degistirinator, FileWriter filewriter){
+void withFile(std::string file1, Degistirinator &manipulator, FileWriter &filewriter){
     std::string line;
     std::ifstream streamIn(file1);
     while (std::getline(streamIn, line)){
-        filewriter.writeLine(degistirinator.replace(line));
+        filewriter.writeLine(manipulator.replace(line));
     }
     streamIn.close();
+
 }
-
-
-int outFile(std::string file, std::string (*contentProvider)()){}
 
 int main(int ac, char* av[]){
     if (ac == 4){
@@ -50,9 +74,9 @@ int main(int ac, char* av[]){
         std::string replacement = av[3];
         std::string newfile = filename + ".replace";
         Degistirinator degistir(substitude, replacement);
-        FileWriter filewriter(newfile);
+        FileWriter writer(newfile);
         
-        withFile(filename, degistir, filewriter);
+        withFile(filename, degistir, writer);
 
 
     }
