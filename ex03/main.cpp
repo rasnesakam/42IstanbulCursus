fa76fbc11389fd661d88b9010a5922e682e842b8
ensar <ensar.makas@gmail.com>
2023-05-16 17:23:49 +0300

    a weapon given to Jim

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -13,6 +13,7 @@ int main()
     {
         Weapon club = Weapon("crude spiked club");
         HumanB jim("Jim");
+		jim.setWeapon(club);
         jim.attack();
         club.setType("some other type of club");
         jim.attack();
