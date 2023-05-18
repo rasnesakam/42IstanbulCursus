bb963a78d1d18815a2587f631a007d13481a33bc
ensar <ensar.makas@gmail.com>
2023-05-18 14:04:42 +0300

    added destructor function to zombie

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -9,6 +9,10 @@ Zombie::Zombie(std::string name){
     this->name = name;
 }
 
+Zombie::~Zombie(){
+	std::cout << "Zombie " << this->name << " has died :(" << std::endl;
+}
+
 void Zombie::anounce(void){
     std::cout << this->name << ":  BraiiiiiiinnnzzzZ..." << std::endl;
 }
