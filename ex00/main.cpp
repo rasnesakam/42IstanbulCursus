4b12b1dc79775858288b504aa090ef7bfea4b93d
ensar <ensar.makas@gmail.com>
2023-07-04 15:10:27 +0300

    checking for evo

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -15,7 +15,7 @@ int main() {
 	const Animal* i = new Cat();
 	std::cout << j->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound! j->makeSound();
+	i->makeSound(); //will output the cat sound!
 	j->makeSound();
 	meta->makeSound();
 	
