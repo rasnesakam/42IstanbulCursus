d975847082dfda1adfe7ce5f9d786e36faff5121
Ensar Makas <ensar.makas@gmail.com>
2023-10-08 16:11:20 +0300

    wow

---
+++ srcs/mariadb/sql/initial_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS 'wordpress'@'%' IDENTIFIED BY 'inception';
+GRANT ALL PRIVILEGES ON wordpress.* to 'wordpress'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@
