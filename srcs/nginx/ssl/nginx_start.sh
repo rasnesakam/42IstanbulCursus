076330156c41643e9e036789f0385b3331d4b4cf
Ensar Makas <ensar.makas@gmail.com>
2023-10-16 12:12:39 +0300

    emanemanemaneman

--- srcs/nginx/ssl/nginx_start.sh
+++ srcs/nginx/ssl/nginx_start.sh
@@ -3,7 +3,7 @@
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.cart -subj "/C=TR/ST=Istanbul/L=Istanbul/O=wordpress/CN=emakas.42.fr";
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=TR/ST=Istanbul/L=Istanbul/O=wordpress/CN=emakas.42.fr";
 
 echo "Nginx: ssl is set up!";
 fi
