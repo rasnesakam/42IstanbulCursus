254a64ca39b3a51650a5adb998d7f7c3edc1f8fe
Ensar Makas <ensar.makas@gmail.com>
2023-10-26 19:36:06 +0300

    files moved into src/requirements

---
+++ srcs/requirements/wordpress/sh/index.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cd /var/www/html;
+
+# if [ ! -f wp-config.php ]
+# 	then
+		echo "Downloading Wordpress.";
+		wp core download --allow-root
+
+		echo "Setting up Wordpress configuration.";
+		wp config create --allow-root \
+			--dbhost=${WORDPRESS_DB_HOST} \
+			--dbname=${WORDPRESS_DB_NAME} \
+			--dbuser=${WORDPRESS_DB_USER} \
+			--dbpass=${WORDPRESS_DB_PASSWORD} && \
+		echo "Installing Wordpress.";
+		wp core install --allow-root \
+			--url=${WORDPRESS_URL} \
+			--title=${WORDPRESS_TITLE} \
+			--admin_user=${WORDPRESS_USER}\
+			--admin_password=${WORDPRESS_PASSWORD}\
+			--admin_email=${WORDPRESS_EMAIL}&& \
+		wp user create --allow-root \
+			${WORDPRESS_USER} ${WORDPRESS_EMAIL}\
+			--user_pass=${WORDPRESS_PASSWORD};
+
+# fi
+
+echo "Executing arguments: $@";
+exec "$@"
